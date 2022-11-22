1ba5a737ae23ae3c645e27d5ad05e9dc2a9bd16b
helena-f <anano123anano@gmail.com>
2022-11-21 16:37:11 -0800

    done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -13,16 +13,16 @@ const About = () => {
 
       <div className="container about__countainer">
 
-        <div className="about__me">
+        {/* <div className="about__me">
           <div className="about__me-image">
             <img src={ME} alt="About Image" />
           </div>
-        </div>
+        </div> */}
 
 
         <div className="about__content">
-          <div className="about__cards">
-              {/* <article className='about__card'>
+          {/* <div className="about__cards">
+              <article className='about__card'>
                 <FaAward className='about__icon' />
                 <h5>Experience</h5>
                 <small>6+ years Programming</small>
@@ -34,18 +34,24 @@ const About = () => {
               </article>
               <article className='about__card'>
                 <FaAward className='about__icon' />
-                <h5>Experience</h5>
-                <small>6+ years Programming</small>
-              </article> */}
-            </div>
+                <h5>Projects</h5>
+                <small>20+ Projects</small>
+              </article>
+            </div> */}
           <p>
-            When I first picked up a BBC micro:bit in 6th grade, methodically learning each function of the board, I knew I loved the problem solving and hands-on learning programming provided.
-            Since then, I have led my robotics team from local to international competitions for FIRST Tech Challenge, researched alongside Lawrence Berkeley National Laboratory scientists, 
-            and coded many projects of my own. Outside of computer science, I also love figure skating, painting, and playing the cello.  
+            When I first picked up a BBC micro:bit in 6th grade,
+             methodically learning each function of the board, 
+            I fell in love the problem solving and hands-on learning programming provided.
+            Since then, I have continued my journey-- leading my robotics team from 
+            local to international competitions for FIRST Tech Challenge, 
+            researching alongside Lawrence Berkeley National Laboratory scientists, 
+            and coding many projects of my own. 
+            Outside of computer science, I also enjoy figure skating, 
+            painting, and playing the cello.  
           </p>
-
-          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
-
+          <div className='center'>
+            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+          </div>
         </div>
       </div>
     </section>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,59 @@
 import React from 'react'
 import './portfolio.css'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import "swiper/css/pagination";
+import { Pagination, Navigation} from "swiper";
+
+import ART1 from '../../assets/1.jpg'
+import ART2 from '../../assets/art/2.jpg'
+import ART3 from '../../assets/art/3.jpg'
+import ART4 from '../../assets/art/4.jpg'
+import ART5 from '../../assets/art/5.jpg'
+import ART6 from '../../assets/art/6.jpg'
+import ART7 from '../../assets/art/7.jpg'
+
+const data = [
+  {
+    image: ART1,
+    title: 'Dreamer'
+  },
+  {
+    image: ART2,
+    title: 'Hands'
+  },
+  {
+    image: ART3,
+    title: 'Glass'
+  }
+]
+
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
       <h5>Showcase</h5>
       <h2>Portfolio</h2>
+      <h6 id='center'>Swipe</h6>
+      <Swiper className="container portfolio__container"
+      modules={[Pagination, Navigation]}
+      spaceBetween={40}
+      slidesPerView={1}>
+        {
+          data.map(({ image, title }, index) => {
+            return (
+              <SwiperSlide key={index} className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <img src={image}/>
+                </div>
+                <h5 className='art__name'>{title}</h5>
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
     </section>
   )
 }
